4548c153571894f5c754026881f80612d2afba75
Miguel Angel López <miguelangel.lopez@asseco.es>
2022-09-21 13:12:48 +0200

    Create new class Position and refactor other classes to use it

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -1,69 +1,72 @@
 ﻿using System;
 
-namespace TicTacToe;
-
-public class Game
+namespace TicTacToe
 {
-    private readonly Board _board = new();
-    private char _lastPlayer = Tile.EmptyPlayer;
-    const string InvalidFirstPlayer = "Invalid first player";
-    const string InvalidNextPlayer = "Invalid next player";
-    
-    public void Play(char player, int row, int column)
+
+    public class Game
     {
-        ValidatePlay(player, row, column);
+        private readonly Board _board = new();
+        private char _lastPlayer = Tile.EmptyPlayer;
+        const string InvalidFirstPlayer = "Invalid first player";
+        const string InvalidNextPlayer = "Invalid next player";
 
-        UpdateGameState(player, row, column);
-    }
-
-    private void ValidatePlay(char player, int row, int column)
-    {
-        ValidateFirstPlayer(player);
-
-        ValidatePlayerChanges(player);
-
-        _board.ValidateFreePosition(row, column);
-    }
-
-    private void ValidatePlayerChanges(char player)
-    {
-        if (IsSamePlayerAsLastMove(player))
+        public void Play(char player, int row, int column)
         {
-            throw new Exception(InvalidNextPlayer);
-        }
-    }
+            Position position = new Position(row, column);
 
-    private void ValidateFirstPlayer(char player)
-    {
-        if (IfFirstMove() && IsPlayerO(player))
+            ValidatePlay(player, position);
+            UpdateGameState(player, position);
+        }
+
+        private void ValidatePlay(char player, Position position)
         {
-            throw new Exception(InvalidFirstPlayer);
+            ValidateFirstPlayer(player);
+
+            ValidatePlayerChanges(player);
+
+            _board.ValidateFreePosition(position);
         }
-    }
 
-    private void UpdateGameState(char player, int row, int column)
-    {
-        _lastPlayer = player;
-        _board.SetPlayerToTile(player, row, column);
-    }
+        private void ValidatePlayerChanges(char player)
+        {
+            if (IsSamePlayerAsLastMove(player))
+            {
+                throw new Exception(InvalidNextPlayer);
+            }
+        }
 
-    private bool IsSamePlayerAsLastMove(char player)
-    {
-        return player == _lastPlayer;
-    }
+        private void ValidateFirstPlayer(char player)
+        {
+            if (IfFirstMove() && IsPlayerO(player))
+            {
+                throw new Exception(InvalidFirstPlayer);
+            }
+        }
 
-    private bool IsPlayerO(char player)
-    {
-        return player == Board.PlayerO;
-    }
+        private void UpdateGameState(char player, Position position)
+        {
+            _lastPlayer = player;
+            _board.SetPlayerToTile(player, position);
+        }
 
-    private bool IfFirstMove()
-    {
-        return _lastPlayer == Tile.EmptyPlayer;
-    }
+        private bool IsSamePlayerAsLastMove(char player)
+        {
+            return player == _lastPlayer;
+        }
 
-    public char Winner()
-    {
-        return _board.GetWinner();
+        private bool IsPlayerO(char player)
+        {
+            return player == Board.PlayerO;
+        }
+
+        private bool IfFirstMove()
+        {
+            return _lastPlayer == Tile.EmptyPlayer;
+        }
+
+        public char Winner()
+        {
+            return _board.GetWinner();
+        }
     }
 }
